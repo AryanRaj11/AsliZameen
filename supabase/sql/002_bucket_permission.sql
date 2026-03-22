6ffd990629671bfa6e7ab1448087a5ddd40c35a1
Aryan Raj <aryanraj018@gmail.com>
2026-03-22 23:48:41 +0530

    updated sql file

--- supabase/sql/002_bucket_permission.sql
+++ supabase/sql/002_bucket_permission.sql
@@ -1,8 +1,10 @@
 -- file: supabase/migrations/[timestamp]_add_storage_policies.sql
-
 CREATE POLICY "Allow authenticated uploads"
 ON storage.objects FOR INSERT
-TO authenticated, anon
-WITH CHECK (
-  bucket_id IN ('properties_image', 'properties_papers')
-);
+TO authenticated,anon
+WITH CHECK (bucket_id = 'properties_image');
+
+CREATE POLICY "Allow authenticated file uploads"
+ON storage.objects FOR INSERT
+TO authenticated,anon
+WITH CHECK (bucket_id = 'properties_papers');
