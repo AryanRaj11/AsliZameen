abbb70ff92e6c2cca647e42661075edf79196687
Aryan Raj <aryanraj018@gmail.com>
2026-03-26 19:46:47 +0530

    updated sql

--- supabase/sql/002_bucket_permission.sql
+++ supabase/sql/002_bucket_permission.sql
@@ -2,12 +2,12 @@
 DROP POLICY IF EXISTS "Allow authenticated uploads" ON storage.objects;
 DROP POLICY IF EXISTS "Allow authenticated file uploads" ON storage.objects;
 
-CREATE POLICY IF NOT EXISTS "Allow authenticated uploads"
+CREATE POLICY "Allow authenticated uploads"
 ON storage.objects FOR INSERT
 TO authenticated,anon
 WITH CHECK (bucket_id = 'properties_image');
 
-CREATE POLICY IF NOT EXISTS "Allow authenticated file uploads"
+CREATE POLICY "Allow authenticated file uploads"
 ON storage.objects FOR INSERT
 TO authenticated,anon
 WITH CHECK (bucket_id = 'properties_papers');
